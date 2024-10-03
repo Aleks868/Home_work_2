8033b0e5c23be71f69a05d3a4cf298cf6850c2e1
Aleks <lekc868@rambler.ru>
2024-10-03 16:15:36 +0500

    add def sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -9,3 +9,10 @@ def filter_by_state(list_of_dict: list[dict[str, Any]], state: str = "EXECUTED")
     """
     return [d for d in list_of_dict if d.get("state") == state]
 
+
+def sort_by_date(list_of_dicts: list[dict[str, Any]], reverse_list: bool = True) -> list[dict[str, Any]]:
+    """Функция принимает список и сортирует его по убыванию"""
+
+    sorted_list: list[dict[str, Any]] = sorted(list_of_dicts, key=lambda x: x["date"], reverse=reverse_list)
+
+    return sorted_list
